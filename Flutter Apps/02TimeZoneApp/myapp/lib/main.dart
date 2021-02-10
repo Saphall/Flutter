26167fb28a19162938834b324da470bc1eb5cc97
Saphall <safalsakha@gmail.com>
2021-02-10 18:42:16 +0545

    commit

---
+++ Flutter Apps/02TimeZoneApp/myapp/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text('Hello'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+        ),
+      ),
+    ));
